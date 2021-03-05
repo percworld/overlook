3d3fb2b73f4c95dd56b2fcf8fa3bcaa39f374ed2
percworld <percworld@gmail.com>
2021-03-04 21:50:20 -0700

    Add styles

--- src/index.js
+++ src/index.js
@@ -3,8 +3,47 @@
 
 import './css/base.scss';
 import './css/styles.scss';
-
 import './images/zulu_beadwork.jpg'
-import updates from './scripts.js';
-import './Hotel.js';
-import './Customer.js'
+
+import updates from './scripts';
+import Hotel from './Hotel';
+
+const getCustomers = () => {
+  return fetch('http://localhost:3001/api/v1/customers')
+    .then(response => response.json())
+    .catch((err) => alert(`This data is not available.  Server says ${err}`))
+}
+
+
+const getOneCustomer = (id) => {
+  return fetch(`http://localhost:3001/api/v1/customers${id}`)
+    .then(response => response.json())
+    .catch((err) => alert(`This data is not available.  Server says ${err}`))
+}
+
+
+const getRooms = () => {
+  return fetch('http://localhost:3001/api/v1/rooms')
+    .then(response => response.json())
+    .catch((err) => alert(`This data is not available.  Server says ${err}`))
+}
+
+
+const getBookings = () => {
+  return fetch('http://localhost:3001/api/v1/bookings')
+  .then(response => response.json())
+  .catch((err) => alert(`This data is not available.  Server says ${err}`))
+}
+
+const getAllData = () => {
+  Promise.all([getCustomers(), getRooms(), getBookings()])
+  .then(dataSets => {
+    let hotel = new Hotel(dataSets[0], dataSets[1], dataSets[2]);
+    updates.onLoad(hotel);
+  })
+  .catch((err) => alert(`This data is not available.  Server says ${err}`));
+}
+
+getAllData();
+
+export default { getAllData, getOneCustomer };
--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,15 @@
 import Customer from './Customer';
 import $ from 'jquery';
 
+let hotel;
 const updates = {
-  
 
+  onLoad: (repoHotel) => {
+    hotel = repoHotel;
+    //$('.')
+
+
+  }
 }
 
 export default updates;
