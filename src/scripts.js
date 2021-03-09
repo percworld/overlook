741e7457a9e395570c586575c5ad9a9f70aa8e11
percworld <percworld@gmail.com>
2021-03-09 12:50:31 -0700

    Add login screen

--- src/scripts.js
+++ src/scripts.js
@@ -1,34 +1,45 @@
 import Customer from './Customer';
 import Hotel from './Hotel';
+import Inquiry from './Inquiry';
 import $ from 'jquery';
 import  getCustomers from './index'
-import moment from 'moment';
-moment().format();
+// import moment from 'moment';
+// moment().format();
+import datepicker from 'js-datepicker'
 
 
 
-let hotel, user, bookings, rooms;
-
+let hotel, user, bookings, today, rooms, inquiry;
+//today = '2020/03/03'
 const updates = {
 
   onLoad: (repoHotel) => {
     hotel = new Hotel(repoHotel.guests.customers, repoHotel.rooms.rooms, repoHotel.confirmations.bookings);
-    console.log(hotel)
     user = new Customer(hotel.guests[1]);
-    updates.updateInfoCard()
+    updates.showLogin();
+    //updates.updateInfoCard();
+  },
 
+  showLogin: () => {
+    updates.displayLogin();
+    // $('.')
   },
 
   updateInfoCard: () => {
+    updates.displayInfoCard();
+    $('.see-bookings').click(updates.showBookings);
+    const picker = datepicker('#picker')
     $('.greeting').html(`
       <p>Hello, ${user.name}</p>
       <p>and Welcome Back! </p>`);
     bookings = hotel.confirmations;
     rooms = hotel.rooms;
+    inquiry = new Inquiry(rooms, bookings);
+    today = inquiry.getToday();
     user.fillBookings(bookings);
     user.fillTotalSpent(rooms)
-    $('.investment').text(`$${user.moneySpent}`)
-    $('.last-date').text(`${user.myBookings[user.myBookings.length -1].date.toLocaleString("en-US")}`)
+    $('.investment').text(`$${user.moneySpent.toFixed(2)}`)
+    $('.last-date').text(`${user.myBookings[user.myBookings.length -1].date.toLocaleString("en-US")}`);
     user.myBookings.forEach(booking => {
     $('.bookings').append(`
       <ul class="booking">
@@ -39,6 +50,116 @@ const updates = {
       </ul>
       `)
     });
+    $('.search-button').click(updates.showInputField)
+  },
+
+  displayInfoCard: () => {
+    $('.dropdown').hide();
+    $('.room-list-container').hide();
+    $('.booking-area').hide();
+    $('.info-area').show();
+    $('.see-bookings').show();
+  },
+
+  displayLogin: () => {
+    $('.dropdown').hide();
+    $('.room-list-container').hide();
+    $('.booking-area').hide();
+    $('.info-area').hide();
+    $('.see-bookings').hide();
+    $('.login-container').show();
+    $('footer').hide();
+  },
+
+  showBookings: () => {
+    $('.dropdown').show();
+    $('.see-bookings').click(() => $('.dropdown').hide())
+  },
+
+  showInputField: () => {
+    $('.info-area').hide();
+    $('.booking-area').show();
+    $('.check-button').click(updates.showRoomsAvailable);
+  },
+
+
+  showRoomsAvailable: () => {
+    event.preventDefault();
+    $('.booking-area').hide();
+    $('.room-list-container').show();
+    updates.fillRoomsByInput();
+    $('.home-button').click(updates.updateInfoCard);
+  },
+
+  fillRoomsByInput: () => {
+    $('.see-bookings').hide();
+    inquiry.checkAvailable(bookings, today);
+    let roomsFiltered;
+    if($('#residential').is(':checked')) {
+      roomsFiltered = inquiry.getRoomsByType('residential');
+     }
+    if($('#suite').is(':checked')) {
+      roomsFiltered = inquiry.getRoomsByType('suite')
+     }
+    if($('#junior').is(':checked')) {
+      roomsFiltered = inquiry.getRoomsByType('junior suite')
+    }
+    if($('#single').is(':checked')) {
+      roomsFiltered = inquiry.getRoomsByType('single room')
+    }
+    if(roomsFiltered) {
+      roomsFiltered.forEach((room, index) => {
+        console.log(room)
+      $('.book-button-container').append(`
+        <br>
+        <button type="button" id="${room.number}" class="home-button">Room Number ${room.number}</button>`);
+        let targetRoom = roomsFiltered[index]
+        //$(`#${index}`).click(updates.assignRoom(targetRoom.number));
+      });
+      const bookButton = document.querySelector('.book-button-container');
+      bookButton.addEventListener('click', updates.assignRoom)
+      //$('#${id}').addEventListener('click', assignRoom(id));
+    } else {
+      $('.book-button-container').text(`We apologize fiercely! No rooms are
+       available on this date that match your wishes!`)
+    }
+    updates.evaluateDateInput;
+  },
+
+  assignRoom: (event) => {
+    console.log('Before:', event.target.id)
+    let roomNumber = event.target.id;
+    console.log('Num : ', roomNumber)
+    if(event.target.classList.contains("home-button")) {
+      roomNumber = event.target.id;
+    }
+    const payload = { "userID": user.id, "date": today, "roomNumber": parseInt(roomNumber) }
+    console.log(payload);
+    const addBooking = () => {
+      return fetch('http://localhost:3001/api/v1/bookings',
+        {
+          method:'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(payload),
+        })
+        .then(response => response.json())
+        .then(response => updates.postNotice(response, roomNumber))
+        .catch((err) => alert(`This booking was not added.  Server says ${err}`));
+    }
+    addBooking()
+
+  },
+
+  postNotice: (response, roomNumber) => {
+    setTimeout(() => {
+      $('.book-button-container').text(`Thank you ${user.name}, You are booked for Room number ${roomNumber} on ${response}.  `)
+    }, 1500)
+  },
+
+  evaluateDateInput: () => {
+    console.log('DATE: ', $('.picker val()'))
   }
 }
 
